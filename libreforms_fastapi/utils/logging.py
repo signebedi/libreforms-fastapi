23ebc5b2e0512087be6a2833e1e8640fe7633beb
signebedi <signe@siftingwinnowing.com>
2024-03-24 10:19:36 -0500

    Modified: logger writes to env-specific files (#26)

--- libreforms_fastapi/utils/logging.py
+++ libreforms_fastapi/utils/logging.py
@@ -1,15 +1,16 @@
 import os, logging
 
 
-def set_logger(environment, log_file_name, namespace, log_directory=os.path.join(os.getcwd(), 'instance', 'log')):
+def set_logger(environment, log_file_name, namespace, log_directory=os.path.join(os.getcwd(), 'instance', 'log'), write_to_file:bool=True):
     logger = logging.getLogger(namespace)
     logger.setLevel(logging.INFO)
 
-    if environment == "production":
+    # if environment == "production":
+    if write_to_file and environment != "testing":
         os.makedirs(log_directory, exist_ok=True)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-        log_file = os.path.join(log_directory, log_file_name)
+        log_file = os.path.join(log_directory, environment+"_"+log_file_name)
         file_handler = logging.handlers.RotatingFileHandler(
             log_file, 
             maxBytes=10485760,
