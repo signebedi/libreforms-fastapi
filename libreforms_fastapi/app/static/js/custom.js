22a52df216372ac1ab185e4bccb7c8dd1b4d698f
signebedi <signe@siftingwinnowing.com>
2024-07-03 08:57:11 -0500

    Added: client tz data stripping function (#296)

--- libreforms_fastapi/app/static/js/custom.js
+++ libreforms_fastapi/app/static/js/custom.js
@@ -160,3 +160,30 @@ function prettifyTimeDiff(dateTimeStr, tz="America/New_York") {
   }
 }
 
+
+function formatDate(
+    dateString, 
+    timeZone = 'America/New_York',
+    locale = 'en-US',
+    year = 'numeric',
+    month = '2-digit',
+    day = '2-digit',
+    hour = '2-digit',
+    minute = '2-digit',
+    second = '2-digit'
+) {
+    // Convert string to Date object
+    const date = new Date(dateString);
+    const options = {
+        year: year,
+        month: month,
+        day: day,
+        hour: hour,
+        minute: minute,
+        second: second,
+        timeZone: timeZone
+    };
+
+    const formatter = new Intl.DateTimeFormat(locale, options);
+    return formatter.format(date);
+}
